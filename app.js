bbcad2ff1db320a4b5fe9a64fe1382c3bc98ecdf
porgokarim1 <60364286+porgokarim1@users.noreply.github.com>
2023-08-02 21:14:27 -0400

    Js file list track updated | css redefined

---
+++ app.js
@@ -0,0 +1,178 @@
+// DOM Element Selection
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+// Track List and Player State Variables
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "Hello",
+    artist: "Adele",
+    image: "images/adele_pic.png",
+    path: "musics/Adele_Hello.mp3"
+  },
+  {
+    name: "Don't worry be happy",
+    artist: "Bobby McFerrin",
+    image: "images/Bobby-McFerrin.jpg",
+    path: "musics/Bobby_McFerrin_-_Dont_Worry_Be_Happy.mp3",
+  },
+  {
+    name: "GIRLFRIEND",
+    artist: "Ruger",
+    image: "images/ruger.jpg",
+    path: "musics/Ruger_Girlfriend.mp3",
+  },
+  {
+    name: "Eh God Barnabas",
+    artist: "Kizz Daniel",
+    image: "images/kizz-daniel.png",
+    path: "musics/Kizz_Daniel_-_Eh_God_Barnabas.mp3",
+  }
+];
+
+
+// Function to generate random background color
+function random_bg_color() {
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
+
+  // Construct a color with the given values
+  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+  // Set the background to that color
+  document.body.style.background = bgColor;
+}
+
+// Function to load and play the selected track
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = "SONG " + (track_index + 1) + " / " + track_list.length;
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
+}
+
+// Function to reset track values
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+// Function to handle play/pause button click
+function playpauseTrack() {
+  if (!isPlaying) {
+    playTrack();
+  } else {
+    pauseTrack();
+  }
+}
+
+// Function to play the track
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+
+// Function to pause the track
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+
+// Function to play the next track
+function nextTrack() {
+  if (track_index < track_list.length - 1) {
+    track_index += 1;
+  } else {
+    track_index = 0;
+  }
+  loadTrack(track_index);
+  playTrack();
+}
+
+// Function to play the previous track
+function prevTrack() {
+  if (track_index > 0) {
+    track_index -= 1;
+  } else {
+    track_index = track_list.length;
+  }
+  loadTrack(track_index);
+  playTrack();
+}
+
+// Function to seek to a specific position in the track
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+// Function to set the volume of the player
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+// Function to update the seek slider and current time
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
